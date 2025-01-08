14524fbc7fd55578ddaca492f951687aee0243d6
PriyaBPangavhane <priyapangavhane99@gmail.com>
2025-01-08 21:02:58 +0530

    5th commit

--- src/jan05_CoreJavaPractice/demo2.java
+++ src/jan05_CoreJavaPractice/demo2.java
@@ -3,5 +3,6 @@ package jan05_CoreJavaPractice;
 public class demo2 {
     public static void main(String[] args) {
         System.out.println("PriyaP");
+        System.out.println("Ajinkya");
     }
 }
