f8b81c1c0798fe0ffcd010716b4ac027be3d3eae
PriyaBPangavhane <priyapangavhane99@gmail.com>
2025-02-06 06:46:29 +0530

    30th Jan Task

--- src/jan30th_Tasks/Smallest_Number_2.java
+++ src/jan30th_Tasks/Smallest_Number_2.java
@@ -1,4 +1,22 @@
 package jan30th_Tasks;
 
+import java.util.Scanner;
+
 public class Smallest_Number_2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the two Numbers");
+        int Num1 = sc.nextInt();
+        int Num2 = sc.nextInt();
+
+        if(Num1<Num2)
+        {
+            System.out.println("Smallest Number is "+Num1);
+        }
+        else
+        {
+            System.out.println("Smallest Number is "+Num2);
+        }
+    }
+
 }
